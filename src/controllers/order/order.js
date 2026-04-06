88c22d659963bedcd65fa572f4c5e38476ae07c5
I-Mall-Project <shamimrahman920@gmail.com>
2026-04-06 22:55:14 +0600

    updated

--- src/controllers/order/order.js
+++ src/controllers/order/order.js
@@ -18,6 +18,7 @@ import sendTelegramMessage from "../../utils/Sendtelegram.js";
 import { autoAssignRider } from "../../utils/assignRider.js";
 import { calculateDeliveryCharge } from "../../utils/Deliverycharge.js";
 import { generateInvoiceNumber } from "../../utils/generateInvoiceNumber.js";
+import { checkAndSendLoyaltyCoupon } from "../coupon/coupon.js";
 
 
 
